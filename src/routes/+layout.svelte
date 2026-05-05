c4f294b716c8f41538a8ba883e3f4c7f69281f11
Trplnr <87922281+Trioplane@users.noreply.github.com>
2026-05-05 21:29:46 +0800

    fix build bugs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 	<title>Community LegitiAwards</title>
 	<meta property="og:title" content="The Community LegitiAwards" />
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="{page.url.hostname}" />
+	<meta property="og:url" content="{page.url.origin}" />
 	<meta property="og:image" content="/assets/img/press_kit/community_legitiawards_banner.png" />
 </svelte:head>
 
